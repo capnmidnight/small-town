22c783519c49f5805546d35ecca8b786e3071feb
Sean T. McBeth <sean.mcbeth@gmail.com>
2013-11-18 10:33:27 -0500

    items can be removed from rooms now

--- play.rkt
+++ play.rkt
@@ -1,23 +1,33 @@
 #lang racket
-(struct exit (name id key lock-msg) #:transparent)
-(struct item (name descrip) #:transparent)
-(struct room (name descrip items exits) #:transparent)
-(struct body (name location items) #:transparent #:mutable)
+(struct exit (name room-id key lock-msg) #:transparent)
+(struct item (name descrip count) #:transparent #:mutable)
+(struct room (name descrip items exits) #:transparent #:mutable)
+(struct body (name location items pc?) #:transparent #:mutable)
 
 (define (item-description itm)
-  (format "~n\t~a - ~a" 
+  (format "~n\t~a ~a - ~a"
+          (item-count itm)
           (item-name itm) 
           (item-descrip itm)))
 
+(define (room-filename x)
+  (format "~a.room" (exit-room-id x)))
+
+(define (room-full-items rm)
+  (let ([items (room-items rm)])
+    (and items 
+         (filter (compose positive? item-count) 
+                 items))))
+
 (define (exit-description x)
-  (let* ([f (format "~a.room" (exit-id x))]
-         [exists (file-exists? f)]
+  (let* ([filename (room-filename x)]
+         [exists (file-exists? filename)]
          [extra (if exists "" " (UNDER CONSTRUCTION)")])
     (format "~n\t~a~a" 
             (exit-name x) 
             extra)))
 
-(define (name-list f lst)
+(define (list-descriptions f lst)
   (let ([strs (and lst (map f lst))])
     (or (and strs (string-join strs)) "\n\tnone")))
 
@@ -31,14 +41,20 @@ ITEMS:~a
 EXITS:~a" 
           (room-name rm) 
           (room-descrip rm)
-          (name-list item-description (room-items rm))
-          (name-list exit-description (room-exits rm))))
+          (list-descriptions item-description (room-full-items rm))
+          (list-descriptions exit-description (room-exits rm))))
+
+(define current-rooms (make-hash))
 
 (define (read-room id)
-  (let ([filename (string-append (symbol->string id) ".room")])
-    (with-input-from-file filename
-      (λ ()
-        (eval (read))))))
+  (hash-ref! current-rooms id
+             (λ ()
+               (let ([filename (string-append (symbol->string id) ".room")])
+                 (with-input-from-file filename
+                   (λ ()
+                     (eval (read))))))))
+
+
 
 (define (with-room rm-id thunk)
   (let* ([filename (string-append (symbol->string rm-id) ".room")]
@@ -46,7 +62,7 @@ EXITS:~a"
          [rm (and exists (read-room rm-id))])
     (if rm
         (thunk rm)
-        (display (format "Room \"~a\" doesn't exist" rm-id)))))
+        (displayln (format "Room \"~a\" doesn't exist" rm-id)))))
 
 (define (write-room id rm)
   (let ([filename (string-append (symbol->string id) ".room")])
@@ -57,28 +73,35 @@ EXITS:~a"
       #:exists 'replace)))
 
 (define (show-room id)
-  (with-room id (λ (rm) (display (room-description rm)))))
+  (with-room id (λ (rm) (displayln (room-description rm)))))
 
 (define current-location 'test)
 (define current-items '())
+(define current-cmds '(quit look take north south east west))
+(define done #f)
+
+(define (quit)
+  (set! done #t))
 
 (define (look)
   (show-room current-location))
 
 (define (move dir)
-  (with-room current-location
-             (λ (rm)
-               (let* ([exits (room-exits rm)]
-                      [xs (and exits (memf (λ (y) (eq? (exit-name y) dir)) exits))]
-                      [x (and xs (car xs))]
-                      [exists (and x (file-exists? (format "~a.room" (exit-id x))))]
-                      [key (and exists (exit-key x))]
-                      [good (or (not key) (and key (member key current-items)))])
-                 (if good
-                     (begin (set! current-location (exit-id x)) (look))
-                     (if key
-                         (display (exit-lock-msg x))
-                         (display "You can't go that way")))))))
+  (with-room 
+   current-location
+   (λ (rm)
+     (let* ([exits (room-exits rm)]
+            [x (and exits (findf (λ (y) (equal? (exit-name y) dir)) exits))]
+            [exists (and x (file-exists? (room-filename x)))]
+            [key (and exists (exit-key x))]
+            [good (and x (or (not key) (and key (member key current-items))))])
+       (if good
+           (begin 
+             (set! current-location (exit-room-id x)) 
+             (look))
+           (if key
+               (displayln (exit-lock-msg x))
+               (displayln "You can't go that way")))))))
 
 (define (north) (move 'north))
 (define (east) (move 'east))
@@ -86,16 +109,32 @@ EXITS:~a"
 (define (west) (move 'west))
 
 (define (take itm)
-  (with-room current-location
-             (λ (rm)
-               (let* ([items (room-items rm)]
-                      [is (and items (memf (λ (y) (eq? (item-name y) itm)) items))]
-                      [i (and is (car is))])
-                 (if i 
-                     (begin (set! current-items (cons itm current-items))
-                            (display (format "You picked up the ~a" itm)))
-                     (display "there is nothing here like that"))))))
+  (with-room 
+   current-location
+   (λ (rm)
+     (let* ([items (room-full-items rm)]
+            [i (and items (findf (λ (y) (eq? (item-name y) itm)) items))])
+       (if i 
+           (begin
+             (set-item-count! i (sub1 (item-count i)))
+             (set! current-items (cons itm current-items))
+             (displayln (format "You picked up the ~a" itm)))
+           (displayln "there is nothing here like that"))))))
 
+(define (do-command str)
+  (let* ([parts (map string->symbol (string-split str " "))]
+         [cmd (first parts)]
+         [pms (rest parts)]
+         [exists (member cmd current-cmds)]
+         [proc (and exists (eval cmd))]
+         [arg-count (or (and proc (procedure-arity proc)) 0)])
+    (if exists
+        (if (= (length pms) arg-count)
+            (apply proc pms)
+            (if (< (length pms) arg-count)
+                (displayln "not enough arguments")
+                (displayln "too many arguments")))
+        (displayln (format "I do not understand \"~a\"." cmd)))))
 
 (define (create-test-rooms)
   (write-room 'test (room "a test room"
@@ -104,10 +143,10 @@ This is just a test room.
 It's meant for testing.
 Nothing more.
 Goodbye."
-                          (list (item 'sword "a rusty sword")
-                                (item 'bird "definitely a bird")
-                                (item 'rock "definitely not a bird")
-                                (item 'garbage "some junk"))
+                          (list (item 'sword "a rusty sword" 1)
+                                (item 'bird "definitely a bird" 1)
+                                (item 'rock "definitely not a bird" 5)
+                                (item 'garbage "some junk" 0))
                           (list (exit 'north 'test2 #f #f)
                                 (exit 'east 'test3 #f #f)
                                 (exit 'south 'test4 'bird "don't forget the bird"))))
@@ -131,3 +170,10 @@ Goodbye."
                            "it's probably going to work"
                            #f
                            (list (exit 'west 'test4 #f #f)))))
+
+
+(define (run)
+  (set! done #f)
+  (let loop ([str (read-line)])
+    (do-command str)
+    (unless done (loop (read-line)))))
