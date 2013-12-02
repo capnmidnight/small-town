015b3d420e1fae9e526accfff15a70d89339236b
Sean T. McBeth <sean.mcbeth@gmail.com>
2013-12-02 17:58:29 -0500

    serving static files and handling server dropouts

--- html5/server.js
+++ html5/server.js
@@ -47,36 +47,69 @@ var loop = function ()
 };
 timer = setInterval(loop, 100);
 
+var mimeTypes = {
+    "html": "text/html",
+    "png": "image/png"
+}
+
 function handler(req, res)
 {
-    fs.readFile(__dirname + "/index.html",
-    function(err, data)
+    console.log("REQUEST:", req.method, req.url);
+    if (req.method === "GET" && req.url[0] === "/")
     {
-        if(err)
+        if (req.url.length == 1)
+            req.url += "index.html";
+        var path = __dirname + req.url;
+        fs.readFile(path,
+        function (err, data)
         {
-            res.writeHead(500);
-            res.end("error loading index.html");
-        }
-        else
-        {
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(data);
-        }
-    });
+            if (err)
+            {
+                serverError(res, req.url);
+            }
+            else
+            {
+                var ext = path.substring(path.indexOf(".") + 1);
+                res.writeHead(200, { "Content-Type": mimeTypes[ext] || "text/plain" });
+                res.end(data);
+            }
+        });
+    }
+    else
+    {
+        serverError(res);
+    }
+}
+
+function serverError(res, path)
+{
+    if (path)
+    {
+        res.writeHead(404);
+        res.end("error loading " + path.substring(1));
+    }
+    else
+    {
+        res.writeHead(500);
+        res.end("error");
+    }
 }
 
 io.sockets.on("connection", function(socket)
 {
     socket.on("name", function(name)
     {
-        if(!serverState.users[name])
+        console.log("naming", name);
+        if(serverState.users[name])
         {
-            serverState.users[name] = new Body("welcome", 100, {"gold": 10}, null, name, socket);
-            socket.emit("good name", name);
+            console.log("old name");
+            socket.emit("news", "Name is already in use, try another one.");
         }
         else
         {
-            socket.emit("news", "Name is already in use, try another one.");
+            console.log("new name");
+            serverState.users[name] = new Body("welcome", 100, { "gold": 10 }, null, name, socket);
+            socket.emit("good name", name);
         }
     });
     socket.emit("news", "Please enter a name.");
