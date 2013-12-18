9aab07c2bfb45edaf1de8b5515336783591eeced
Sean T. McBeth <sean.mcbeth@gmail.com>
2013-12-18 15:08:46 -0500

    #26: some tests for Body.cmd_drink

--- test/body.js
+++ test/body.js
@@ -3,11 +3,26 @@ var Body = require("../body.js");
 describe("Body", function() {
 	var roomId = "test";
 	var hp = 25;
+	var potionStrength = 10;
 	var body = new Body(roomId, hp); // items, equipment, id, socket)
+	body.db = {
+		"itemCatalogue":{
+			"potion": {
+				"equipType": "food",
+				"strength": potionStrength
+			}
+		}
+	};
 	
 	describe("items", function(){
 		it("should not start null", function(){
-			assert.isNotNull(body.items);
+			assert.ok(body.items);
+		});
+	});
+	
+	describe("equipment", function(){
+		it("should not start null", function(){
+			assert.ok(body.equipment);
 		});
 	});
 	
@@ -15,12 +30,20 @@ describe("Body", function() {
 		it("should start out at the test value", function(){
 			assert.equal(body.hp, hp);
 		});
-		
-		it("should increase when drinking a potion", function(){
+	});
+	
+	describe("cmd_drink", function(){		
+		it("should increase hp when drinking a potion", function(){
 			body.items.potion = 1;
 			var startHP = body.hp;
 			body.cmd_drink("potion");
-			assert.equal(body.hp, startHP + 10);
+			assert.equal(body.hp, startHP + potionStrength);
+		});
+		
+		it("should decrease potion count", function(){
+			body.items.potion = 2;
+			body.cmd_drink("potion");
+			assert.equal(body.items.potion, 1);
 		});
 	});
 });
